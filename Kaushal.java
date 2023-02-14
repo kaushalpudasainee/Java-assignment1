42112b129b90d673d024a5ba7afc152d556c0fa1
kaushalpudasinee <kaushalpudasainee7@gmail.com>
2023-02-14 09:27:19 +0545

    first code

---
+++ Kaushal.java
@@ -0,0 +1,13 @@
+public class Kaushal{
+    
+    public static void main(String[] args){
+        System.out.println("Name of the Organization:Pudasainee Airlines\n" +"\n" +
+                         "Address:\n" +
+                         "----State: Bagmati\n" +
+                         "----Zip Code: 4460\n" +
+                         "----Country: Nepal\n" +"\n" +
+                         "Contact Information:\n" +
+                         "----Mobile number: 9840389926\n" +
+                         "----Email: kaushalpudasainee7@gmail.com");
+    }
+}
